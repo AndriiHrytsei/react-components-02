ecc4930482fc32cb7482405892c4d7042530cd8f
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2023-06-09 17:55:07 +0200

    add some css

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable react/prop-types */
+import css from "./profile.module.css"
+console.log(css);
 function Profile ({ username, tag, location, avatar, stats }) {
     return (
         <>
-        <div className="profile">
-            <div className="description">
+        <div className={css.profile}>
+            <div className={css.description}>
                 <img
                     src={avatar}
                     alt="User avatar"
-                    className="avatar"
+                    className={css.avatar}
                 />
-                <p className="name">{username}</p>
-                <p className="tag">@{tag}</p>
+                <p className={css.name}>{username}</p>
+                <p className={css.tag}>@{tag}</p>
                 <p className="location">{location}</p>
             </div>
 
